d2b09414c1990ed1e1c44317338f6b9f744053b5
Amrut Mishra <mishra.amrut90@gmail.com>
2016-06-29 15:20:13 +0530

    removed alert
    one extra alert was there. i removed it

--- www/playstore.js
+++ www/playstore.js
@@ -1,6 +1,5 @@
 function openPlaystoreApplication(){
 	this.openPlaystoreApp = function(application_package_name){
-		alert("About to call");
 		cordova.exec(function(){},function(){},"playstore","openPlaystoreApp",[application_package_name]);
 	}
 }
